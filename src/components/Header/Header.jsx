81b1ed42e310d0edf28a7a06f64254dacd06c167
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-05-18 15:38:40 +0200

    Added dark and light mode images (SVG) as switches to one or the other state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import SunsetImg from '../../assets/images/sunset.svg';
+import SunriseImg from '../../assets/images/sunrise.svg';
 import './Header.css';
 
 function Header() {
 
   let myHeaderMode = "light";
   let myHeaderClassMode = `myHeader myHeader-${myHeaderMode}`;
-  let myHeaderModeSwitchImage = myHeaderMode === "light" ? ImgDarkMode : ImgLightMode;
+  let myHeaderModeSwitchImage = myHeaderMode === "light" ? SunsetImg : SunriseImg;
   let myContraryMode = myHeaderMode === "light" ? "dark" : "light";
 
   return (
@@ -20,15 +22,10 @@ function Header() {
         <Link className='myHeaderLink' to='/works'>Nos projets</Link>
       </div>
       <div>
-        <img src={source} onClick={onClick} id='theme' className="rotationReverse"></img>
         <button>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sunrise" viewBox="0 0 16 16">
-            <path d="M7.646 1.146a.5.5 0 0 1 .708 0l1.5 1.5a.5.5 0 0 1-.708.708L8.5 2.707V4.5a.5.5 0 0 1-1 0V2.707l-.646.647a.5.5 0 1 1-.708-.708l1.5-1.5zM2.343 4.343a.5.5 0 0 1 .707 0l1.414 1.414a.5.5 0 0 1-.707.707L2.343 5.05a.5.5 0 0 1 0-.707zm11.314 0a.5.5 0 0 1 0 .707l-1.414 1.414a.5.5 0 1 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zM8 7a3 3 0 0 1 2.599 4.5H5.4A3 3 0 0 1 8 7zm3.71 4.5a4 4 0 1 0-7.418 0H.499a.5.5 0 0 0 0 1h15a.5.5 0 0 0 0-1h-3.79zM0 10a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 0 10zm13 0a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
-          </svg>
+          <img src={myHeaderModeSwitchImage}></img>
           &nbsp;Switch to {myContraryMode} mode&nbsp;
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sunset" viewBox="0 0 16 16">
-            <path d="M7.646 4.854a.5.5 0 0 0 .708 0l1.5-1.5a.5.5 0 0 0-.708-.708l-.646.647V1.5a.5.5 0 0 0-1 0v1.793l-.646-.647a.5.5 0 1 0-.708.708l1.5 1.5zm-5.303-.51a.5.5 0 0 1 .707 0l1.414 1.413a.5.5 0 0 1-.707.707L2.343 5.05a.5.5 0 0 1 0-.707zm11.314 0a.5.5 0 0 1 0 .706l-1.414 1.414a.5.5 0 1 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zM8 7a3 3 0 0 1 2.599 4.5H5.4A3 3 0 0 1 8 7zm3.71 4.5a4 4 0 1 0-7.418 0H.499a.5.5 0 0 0 0 1h15a.5.5 0 0 0 0-1h-3.79zM0 10a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 0 10zm13 0a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
-          </svg>
+          <img src={myHeaderModeSwitchImage}></img>
         </button>
       </div> 
     </header>
